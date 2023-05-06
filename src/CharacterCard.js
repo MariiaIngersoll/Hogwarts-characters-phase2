ed71cbf878ce8167ad9460d558b5a97a0ad0cf1f
+ <+>
2023-05-05 22:55:12 -0400

    created component that is gonna render a single character card

---
+++ src/CharacterCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function CharacterCard() {
+    return (
+        <li className="card">
+            <div className="image">
+                <img />
+                <h2>name</h2>
+                <h3>house?</h3>
+
+            </div>
+        </li>
+    )
+}
+
+export default CharacterCard
