2dee743886aaad32d142466710ebca898eb7da37
+ <+>
2023-05-05 22:16:18 -0400

    added fetch and was able to console log characters

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React , { setState, useEffect, useState } from "react";
+import { Route, Routes, useSearchParams } from "react-router-dom";
 import AllCharacters from "./AllCharacters";
 import HogwartsHouses from "./HogwartsHouses"
 import NavBar from "./NavBar"
@@ -9,12 +9,22 @@ import Search from "./Search"
 
 function App() {
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:4000/characters")
+    .then((r) => r.json())
+    .then(data => setCharacters(data))
+  })
+
   return (
-    <div>
-      
-     <Routes>
-      <Route exact path="/" element={<NavBar />} />
-      <Route path="/houses" element={<HogwartsHouses />} />
+    <div >
+      <header className="header">
+        <h1>Welcome to The Hogwarts World</h1>
+      </header>
+       <NavBar />
+       <Routes>
+      <Route path="/" element={<HogwartsHouses />} />
       <Route path="/characters" element={<AllCharacters />} />
       
      </Routes>
