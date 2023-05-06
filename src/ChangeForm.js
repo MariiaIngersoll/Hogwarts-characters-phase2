e7ea7895f8f70b69d7f3fea35e2fff67501020f8
+ <+>
2023-05-05 23:52:59 -0400

    createn onChange form with Hogwarts houses names

---
+++ src/ChangeForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ChangeForm() {
+    return (
+        <select id="selectMenu">
+            <option value="none">Choose a Hogwarts House</option>
+            <option value="Gryffindor">Gryffindor</option>
+            <option value="Hufflepuff">Hufflepuff</option>
+            <option value="Ravenclaw">Ravenclaw</option>
+            <option value="Slytherin">Slytherin</option>
+      </select>
+    )
+}
+
+export default ChangeForm
