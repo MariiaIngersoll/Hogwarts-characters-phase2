d3e3e1ac2f75179e885b7d28e8686484aa73ee60
+ <+>
2023-05-06 00:59:00 -0400

    added a logo for each hogwarts house

--- src/HogwartsHouses.js
+++ src/HogwartsHouses.js
@@ -2,8 +2,29 @@
 function HogwartsHouses() {
     return (
         <div>
-            <h2>Hey</h2>
+            <br></br>
+            <h1 id="headerForHouses">Hogwarts Houses</h1>
+            <br></br>
+            <div className="housesContainer">
+            
+                <span className="span">
+                <h2>Gryffindor</h2> 
+                <img src="https://i.ebayimg.com/images/g/Jv4AAOSwPV9a44tr/s-l500.jpg" />
+                </span>
+                <span className="span">
+                    <h2>Ravenclaw</h2>
+                    <img src="https://cdn11.bigcommerce.com/s-ydriczk/images/stencil/1280x1280/products/88363/91130/Harry-Potter-Ravenclaw-Crest-Official-wall-mounted-cardboard-cutout-buy-now-at-star__86173.1507642983.jpg?c=2" />
+                </span>
+                <span className="span">
+                    <h2>Hufflepuff</h2>
+                    <img src="https://i.ebayimg.com/images/g/KMQAAOSwS5lfh0Rl/s-l500.jpg" />
+                </span>
+                <span className="span">
+                    <h2>Slytherin</h2>
+                    <img src="https://i.ebayimg.com/images/g/C0gAAOSwQnFdPJRl/s-l500.jpg" />
+                </span>
         </div>
+     </div>
     )
 }
 
