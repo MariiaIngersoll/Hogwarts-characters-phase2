c0300635ab9b9c7e108fecb65e1e96bde2f6672f
+ <+>
2023-05-06 20:35:28 -0400

    display characters by their houses

--- src/AllCharacters.js
+++ src/AllCharacters.js
@@ -1,13 +1,20 @@
-import React from "react";
-import CharacterCard from "./CharacterCard"
+import React, { useState, useEffect } from "react";
+import CharacterCard from "./CharacterCard";
+import ChangeForm from "./ChangeForm";
+
 
 function AllCharacters( { characters }) {
-    console.log(characters)
-    const listOfCharacters = characters.map((character) => {
+
+    const [filterBy, setFilterBy] = useState("none");
+    const filteredCharacters = characters.filter((char) => char.house === filterBy)
+    console.log(filteredCharacters)
+
+    const listOfCharacters = filteredCharacters.map((character) => {
        return <CharacterCard key={character.id} character={character} />
     })
     return (
         <main>
+            <ChangeForm filterCharacter={filteredCharacters} setFilterBy={setFilterBy} filterBy={filterBy} characters={filteredCharacters}/>
             <ul className="listCharacters">
                 {listOfCharacters}
             </ul>
