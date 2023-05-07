cf5f3bb93afa4f3c1526317860750b618a61c5fb
+ <+>
2023-05-06 22:06:18 -0400

    created delete button

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CharacterCard( { character }) {
+
+function CharacterCard( { character, onDeleteItem }) {
+    const [favButton, setFavButton] = useState(false)
+
+    const favoriteButton = () => {
+        setFavButton(!favButton)
+    }
+
+    function handleDeleteClick() {
+        fetch(`http://localhost:4000/characters/${character.id}`, {
+            method: "DELETE",
+          })
+            .then((r) => r.json())
+            .then(() => onDeleteItem(character));
+        }
+    
+
     return (
         <li className="card">
             <div className="image">
@@ -9,8 +25,15 @@ function CharacterCard( { character }) {
                 <h3>{character.house}</h3>
 
             </div>
+            <div className="details">
+            {favButton ? (
+                <button onClick={favoriteButton} >❤</button>
+                ) : (
+                <button onClick={favoriteButton} >♡</button>
+                )}
+            <button  onClick={handleDeleteClick}>Delete</button>
+            </div>
         </li>
     )
-}
-
+                }
 export default CharacterCard
